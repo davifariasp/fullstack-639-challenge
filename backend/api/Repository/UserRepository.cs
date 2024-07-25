dc8111648598d4835863a624d758b5e5a6f92d35
davifariasp <davi.informatica8@gmail.com>
2024-07-24 21:17:58 -0300

    feature: adicionando metodos de insercao do redis no repositorio usuario

--- backend/api/Repository/UserRepository.cs
+++ backend/api/Repository/UserRepository.cs
@@ -7,16 +7,20 @@ using api.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using api.Mappers;
+using Microsoft.Extensions.Caching.Distributed;
+using api.Dtos.User;
 
 namespace api.Interfaces.Repository
 {
     public class UserRepository : IUserRepository
     {   
         private readonly ApplicationDBContext _context;
+        private readonly IDistributedCache _cache;
 
-        public UserRepository(ApplicationDBContext context)
+        public UserRepository(ApplicationDBContext context, IDistributedCache cache)
         {
             _context = context;
+            _cache = cache;
         }
 
         public async Task<User> CreateAsync(User user)
@@ -41,6 +45,38 @@ namespace api.Interfaces.Repository
         public async Task<User?> GetByEmailAsync(string email)
         {
             return await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
+        }
+
+        //usuarios no redis
+        public void setUserOnline(int id, string lat, string lon, string tokenDevice)
+        {
+            var user = new
+            {
+                id,
+                lat,
+                lon,
+                tokenDevice
+            };
+
+            var userJson = System.Text.Json.JsonSerializer.Serialize(user);
+            _cache.SetString("user:" + id, userJson);
+        }
+
+        public List<UserOnlineDto> getUsersOnline()
+        {
+            List<UserOnlineDto> usersOnline = new List<UserOnlineDto>();
+
+            for (int i = 1; i <= 100; i++) // Supondo que o ID máximo do usuário seja 100
+            {
+                var json = _cache.GetString("user:" + i);
+                if (!string.IsNullOrEmpty(json))
+                {
+                    UserOnlineDto user = UserOnlineDto.FromJson(json);
+                    usersOnline.Add(user);
+                }
+            }
+
+            return usersOnline;
         }
     }
 }
