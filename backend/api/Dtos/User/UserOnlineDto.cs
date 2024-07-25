b2f31c71fc53cebed8191818c7d9766c63ff9066
davifariasp <davi.informatica8@gmail.com>
2024-07-24 21:19:44 -0300

    feature: adicionando dto de usuario online

---
+++ backend/api/Dtos/User/UserOnlineDto.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace api.Dtos.User
+{
+    public class UserOnlineDto
+    {
+        public int? Id { get; set; }
+        public double? Lat { get; set; }
+        public double? Lon { get; set; }
+
+        public string? TokenDevice { get; set; }
+
+        // Opcional: método de fábrica estático para criar UserOnlineDto a partir do JSON
+        public static UserOnlineDto FromJson(string json)
+        {
+            return JsonSerializer.Deserialize<UserOnlineDto>(json);
+        }
+    }
+}
